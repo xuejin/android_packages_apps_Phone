bf7a898aec7051ab876c88a2f2877a8fbd417b9c
Quarx2k <agent00791@gmail.com>
2011-07-29 00:38:49 +0700

    CrystalTalk

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -132,6 +132,8 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_VOICEMAIL_PROVIDER_KEY = "button_voicemail_provider_key";
     private static final String BUTTON_VOICEMAIL_SETTING_KEY = "button_voicemail_setting_key";
     private static final String BUTTON_FDN_KEY   = "button_fdn_key";
+    private static final String BUTTON_VOICE_QUALITY_KEY = "button_voice_quality_key";
+    private static String mVoiceQuality;
 
     /**
      * @hide
@@ -215,6 +217,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private ListPreference mButtonTTY;
     private ListPreference mButtonSipCallOptions;
     private ListPreference mVoicemailProviders;
+    private ListPreference mButtonVoiceQuality;
     private PreferenceScreen mVoicemailSettings;
     private SipSharedPreferences mSipSharedPreferences;
 
@@ -508,6 +511,8 @@ public class CallFeaturesSetting extends PreferenceActivity
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == mSubMenuVoicemailSettings) {
             return true;
+        } else if (preference == mButtonVoiceQuality) {
+            return true;
         } else if (preference == mButtonDTMF) {
             return true;
         } else if (preference == mButtonTTY) {
@@ -551,6 +556,8 @@ public class CallFeaturesSetting extends PreferenceActivity
                     Settings.System.DTMF_TONE_TYPE_WHEN_DIALING, index);
         } else if (preference == mButtonTTY) {
             handleTTYChange(preference, objValue);
+	    } else if (preference == mButtonVoiceQuality) {
+            setVoiceQuality((String)objValue);
         } else if (preference == mButtonNotifications) {
             handleNotificationChange(objValue);
         } else if (preference == mVoicemailProviders) {
@@ -591,6 +598,14 @@ public class CallFeaturesSetting extends PreferenceActivity
         }
         // always let the preference setting proceed.
         return true;
+    }
+
+    public String getVoiceQuality() {
+	  return mVoiceQuality;
+    }
+
+    public void setVoiceQuality(String value) {
+	mVoiceQuality = value;
     }
 
     private void handleNotificationChange(Object objValue) {
@@ -1539,6 +1554,18 @@ public class CallFeaturesSetting extends PreferenceActivity
         mButtonHAC = (CheckBoxPreference) findPreference(BUTTON_HAC_KEY);
         mButtonTTY = (ListPreference) findPreference(BUTTON_TTY_KEY);
         mVoicemailProviders = (ListPreference) findPreference(BUTTON_VOICEMAIL_PROVIDER_KEY);
+        mButtonVoiceQuality = (ListPreference) findPreference(BUTTON_VOICE_QUALITY_KEY);
+
+
+        if (mButtonVoiceQuality != null) {
+            if (getResources().getBoolean(R.bool.crystaltalk_enabled) == true) {
+                mButtonVoiceQuality.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mButtonVoiceQuality);
+                mButtonVoiceQuality = null;
+            }
+        }
+
         if (mVoicemailProviders != null) {
             mVoicemailProviders.setOnPreferenceChangeListener(this);
             mVoicemailSettings = (PreferenceScreen)findPreference(BUTTON_VOICEMAIL_SETTING_KEY);
@@ -1688,6 +1715,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         mTrackballAnswer.setValue(mTrackAnswer);
         mTrackballHangup = (ListPreference) prefSet.findPreference(BUTTON_TRACKBALL_HANGUP);
         mTrackballHangup.setValue(mTrackHangup);
+
+            if (getResources().getBoolean(R.bool.crystaltalk_enabled) == true) {
+	mButtonVoiceQuality.setValue(mVoiceQuality); 
+	}
 
         // No reason to show Trackball Answer & Hangup if it doesn't have a
         // Trackball.
@@ -2118,7 +2149,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         mTrackHangup = pref.getString(BUTTON_TRACKBALL_HANGUP, "-1");
         mHideHoldButton = pref.getBoolean(BUTTON_HIDE_HOLD_BUTTON, false);
         mBlackRegex = pref.getBoolean(BUTTON_BLACK_REGEX, false);
-
+	    mVoiceQuality = pref.getString(BUTTON_VOICE_QUALITY_KEY,"Normal");
         ObjectInputStream ois = null;
         boolean correctVer = false;
         try {
@@ -2271,6 +2302,9 @@ public class CallFeaturesSetting extends PreferenceActivity
         outState.putString(BUTTON_TRACKBALL_ANSWER, mTrackballAnswer.getValue());
         outState.putString(BUTTON_TRACKBALL_HANGUP, mTrackballHangup.getValue());
         outState.putBoolean(BUTTON_HIDE_HOLD_BUTTON, mButtonHideHoldButton.isChecked());
+        if (getResources().getBoolean(R.bool.crystaltalk_enabled) == true) {
+	    outState.putString(BUTTON_VOICE_QUALITY_KEY, mButtonVoiceQuality.getValue());
+	    }
         outState.commit();
         init(pref);
         super.onStop();
--- src/com/android/phone/PhoneApp.java
+++ src/com/android/phone/PhoneApp.java
@@ -30,6 +30,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.media.AudioManager;
+import android.media.AudioSystem;
 import android.net.Uri;
 import android.os.AsyncResult;
 import android.os.Binder;
@@ -1261,6 +1262,9 @@ public class PhoneApp extends Application implements AccelerometerListener.Orien
      */
     /* package */ void updatePhoneState(Phone.State state) {
         if (state != mLastPhoneState) {
+            if (getResources().getBoolean(R.bool.crystaltalk_enabled) == true) {
+                AudioSystem.setParameters("motoVoiceQualityConfig="+ mSettings.getVoiceQuality()); 
+            }
             mLastPhoneState = state;
             updateProximitySensorMode(state);
             if (mAccelerometerListener != null) {
